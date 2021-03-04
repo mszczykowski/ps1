54d4350ffa3c7190fb14f8be79d73d723d5b72f2
mszczykowski <67839881+mszczykowski@users.noreply.github.com>
2021-03-04 14:45:42 +0100

    first commit

---
+++ ps1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ps1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            string output;
+            while(true)
+            {
+                
+                Console.WriteLine("Enter integer:");
+                try
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid value!");
+                    continue;
+                }
+                output = "";
+                if(value % 3 == 0)
+                {
+                    output += "fizz";
+                }
+                if(value % 5 == 0)
+                {
+                    output += "buzz";
+                }
+                if (output == "") output += value;
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
